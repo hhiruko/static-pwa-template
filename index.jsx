379214e395c67549940b9505bf3f28a1ee1b00cf
hhiruko <adilet.toktybay@gmail.com>
2025-07-03 11:15:52 +0500

    feat: deploy to github pages

--- index.jsx
+++ index.jsx
@@ -1,6 +1,8 @@
 import { render } from 'preact';
 import { App } from './src/components/App';
 
+document.title = import.meta.env.VITE_APP_NAME;
+
 if (import.meta.env.PROD && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register(import.meta.env.VITE_APP_PATH + '/service-worker.js');
